2d5e25b034a7b95d403916d4a923118a556c1cba
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2018-11-07 12:10:50 +0100

    js - dzialania na obiektach z wykorzystaniem JSON

---
+++ kurs-fed-zadania-domowe/4-js-obiekt-json/js/obiekt-json.js
@@ -0,0 +1,13 @@
+var jsonPracownicy = {
+    "pracownicy":[
+        {"firstName": "Krystian", "lastName": "Dziopa"}, 
+        {"firstName": "Anna", "lastName": "Szapiel"},
+        {"firstName": "Piotr", "lastName": "Żmuda"}
+    ]
+}
+
+console.log(jsonPracownicy);
+
+jsonPracownicy.pracownicy.forEach(function(element, index) {
+    console.log(index + ' ' + element.firstName + ' ' + element.lastName);
+});
