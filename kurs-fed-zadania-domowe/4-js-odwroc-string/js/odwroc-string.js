b02a16b9eadeb352a3a8b119c8822d1b7820d614
Katarzyna Izak <kasik83@gmail.com>
2018-11-19 12:03:54 +0100

    przeniesienie do repo fed

--- kurs-fed-zadania-domowe/4-js-odwroc-string/js/odwroc-string.js
+++
@@ -1,9 +0,0 @@
-function odwrocString(string) {
-    var tablicaZeStringa = string.split('');
-    var odwrocenie = tablicaZeStringa.reverse();
-    var laczenie = odwrocenie.join('');
-    return laczenie;
-}
-
-var napis = odwrocString('Akademia108');
-console.log(napis);
--- kurs-fed-zadania-domowe/4-js-sortuj-string/js/sortuj-string.js
+++
@@ -1,9 +0,0 @@
-function sortujString(string) {
-    var tablicaZeStringa = string.split('');
-    var sortowanie = tablicaZeStringa.sort();
-    var laczenie = sortowanie.join('');
-    return laczenie;
-}
-
-var napis = sortujString('Akademia108');
-console.log(napis);
